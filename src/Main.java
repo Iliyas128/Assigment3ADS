24fa655ecf754134525eb2a52048167f96fff0bf
Iliyas128 <Weking128@gmail.com>
2024-05-16 01:00:12 +0600

    4 commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        BST<Integer, String> bst = new BST<>();
+
+        bst.put(1, "Value 1");
+        bst.put(3, "Value 3");
+        bst.put(7, "Value 7");
+        bst.put(4, "Value 4");
+        bst.put(2, "Value 2");
+
 
         MyHashTable<MyTestingClass, String> table = new MyHashTable<>();
 
